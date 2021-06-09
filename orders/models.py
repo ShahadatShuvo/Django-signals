311cfa87ba43300f0db1c7c4a33e02b4a99c76c2
hossain-shuvo7860 <shahadat@baiust.edu.bd>
2021-06-09 13:02:14 +0600

    updated code included.

--- orders/models.py
+++ orders/models.py
@@ -6,8 +6,8 @@ from cars.models import Car
 class Order(models.Model):
     name = models.CharField(max_length=30)
     cars = models.ManyToManyField(Car)
-    total = models.PositiveIntegerField(blank=True)
-    total_price = models.PositiveIntegerField(blank=True)
+    total = models.PositiveIntegerField(blank=True, null=True)
+    total_price = models.PositiveIntegerField(blank=True, null=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
